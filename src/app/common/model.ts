e6f7d590a57b9b74040bfd4b0fb75816f1231a99
djors <drjors2@gmail.com>
2023-03-10 19:40:06 -0500

    adds urls

--- src/app/common/model.ts
+++ src/app/common/model.ts
@@ -8,25 +8,42 @@ export interface PeriodicElement {
 export interface OrderCols<T> {
   column: keyof T;
   label: string;
+  options: {
+    urlFun: (t: T) => string;
+  };
 }
 
 export const PeColumns: OrderCols<PeriodicElement>[] = [
-  { column: 'position', label: 'Pos' },
-  { column: 'name', label: 'Name' },
-];
-
-
-
+  // ['position', 'Pos'],
+  ['name', 'Name'],
+  ['weight', 'Weight'],
+  [
+    'symbol',
+    'Symbol',
+    (e: PeriodicElement) => 'https://google.com/search?q=' + e.name,
+  ],
+].map(
+  ([column, label, urlFun]) =>
+    ({ column, label, options: { urlFun } } as OrderCols<PeriodicElement>)
+);
 
 export const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+  ['Hydrogen', 1.0079, 'H'],
+  ['Helium', 4.0026, 'He'],
+  ['Lithium', 6.941, 'Li'],
+  ['Beryllium', 9.0122, 'Be'],
+  ['Boron', 10.811, 'B'],
+  ['Carbon', 12.0107, 'C'],
+  ['Nitrogen', 14.0067, 'N'],
+  ['Oxygen', 15.9994, 'O'],
+  ['Fluorine', 18.9984, 'F'],
+  ['Neon', 20.1797, 'Ne'],
+].map(
+  ([name, weight, symbol], ix) =>
+    ({
+      position: ix + 1,
+      name,
+      weight,
+      symbol,
+    } as PeriodicElement)
+);
